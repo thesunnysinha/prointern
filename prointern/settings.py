e6202948051b435d3f72204142cc211eea0b6217
sunny kumar sinha <thesunnysinha@gmail.com>
2022-02-18 03:14:52 +0530

    commit

--- prointern/settings.py
+++ prointern/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT = STATIC_DIR
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #MEDIA
 
--- prointern/urls.py
+++ prointern/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from Signin import views
 from Home import views as views1
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url 
 
 urlpatterns = [
     path('',views1.prointern,),
@@ -37,9 +36,9 @@ urlpatterns = [
     path('entertainment/',include('Entertainment.urls')),
     path('studymaterial/',include('StudyMaterial.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,
+    re_path(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
+    re_path(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
 ]
 
